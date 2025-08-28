b2f756ef1f12a6cd2f49d3a74a443d8f3dbce422
Enceka <enceka@yeah.net>
2025-08-28 12:16:05 +0800

    pearl: blob: fixup android.hardware.security.keymint@1.0-service.mitee

--- extract-files.py
+++ extract-files.py
@@ -32,7 +32,9 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
    	'vendor/bin/hw/android.hardware.security.keymint@1.0-service.mitee': blob_fixup()
 		.replace_needed('android.system.keystore2-V1-ndk_platform.so', 'android.system.keystore2-V1-ndk.so')
-		.replace_needed('android.hardware.security.keymint-V1-ndk_platform.so','android.hardware.security.keymint-V4-ndk.so'),
+		.replace_needed('android.hardware.security.keymint-V1-ndk_platform.so','android.hardware.security.keymint-V4-ndk.so')
+		.replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
+		.replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndkso'),
 
 	('vendor/lib64/libmt_mitee@1.3.so',
 	'vendor/lib64/libkeymint_support.so'): blob_fixup()
