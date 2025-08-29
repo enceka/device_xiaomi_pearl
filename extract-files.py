eae25dfa83dfb977a5dd76c933bbc55b6fd3727a
Enceka <enceka@yeah.net>
2025-08-29 18:05:08 +0800

    pearl: blob: Fixup error: Unresolved symbol: SetTaskProfiles

--- extract-files.py
+++ extract-files.py
@@ -65,6 +65,11 @@ blob_fixups: blob_fixups_user_type = {
 	'vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service': blob_fixup()
     .replace_needed('android.hardware.vibrator-V1-ndk_platform.so','android.hardware.vibrator-V1-ndk.so'),
 
+    ('vendor/lib64/mt6895/libcam.hal3a.so',
+     'vendor/lib64/mt6895/libcam.hal3a.ctrl.so',
+     'vendor/lib64/mt6895/libmtkcam_request_requlator.so'): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+
     ('vendor/lib64/lib3a.ae.pipe.so',
      'vendor/lib64/mt6895/libaaa_toneutil.so',
      'vendor/lib64/mt6895/lib3a.flash.so',
